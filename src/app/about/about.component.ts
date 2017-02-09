d2b284acbe54694d705391c802a4472e686a8a0d
Mohsin Ghani <Mohsinghani.777@gmail.com>
2017-02-09 08:04:48 -0800

    Update about.component.ts

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -15,38 +15,7 @@ export class AboutComponent implements OnInit {
         {
             this.hereUser = true;
         }
-    }
-  projects = [
-            {
-                value: "jquery",
-                label: "jQuery",
-                desc: "the write less, do more, JavaScript library",
-                icon: "jquery_32x32.png"
-            },
-            { 
-                value: "jquery-ui",
-                label: "jQuery UI",
-                desc: "the official user interface library for jQuery",
-                icon: "jqueryui_32x32.png"
-            },
-            {
-                value: "sizzlejs",
-                label: "Sizzle JS",
-                desc: "a pure-JavaScript CSS selector engine",
-                icon: "sizzlejs_32x32.png"
-            }
-        ];
-  
-
+      
   ngOnInit() {
   }
-
-  changeDesc( value, desc ) {
-   for (var i in this.projects) {
-     if (this.projects[i].value == value) {
-        this.projects[i].desc = desc;
-        break; //Stop this loop, we found it!
-     }
-   }
-}
 }
